0216e34e993a3e165833850117846ffa0314864a
ben <boneill@rnet-tech.com>
2022-03-24 13:23:47 -0400

    change rel urls

---
+++ src/utils/vnv.C
@@ -0,0 +1,37 @@
+
+#include <stddef.h>
+#include "VnV.h"
+
+INJECTION_EXECUTABLE(LIBMESH) 
+
+static const char* libmesh_vnv_schema = "{\"type\": \"object\", \"required\":[]}";
+
+/**
+ * Welcome to libmesh. 
+ *
+ *
+ */ 
+INJECTION_OPTIONS(LIBMESH, libmesh_vnv_schema) {
+    return nullptr;
+}
+
+INJECTION_SUBPACKAGE(LIBMESH,PETSC)
+
+
+/** For testing purposes only -- if you are seeing this, i forgot to remove it
+ *
+ */
+
+int libmesh_vnv_test_function(int x) {
+   
+  INJECTION_LOOP_BEGIN("LIBMESH", VWORLD, "SanityCheck", x);
+  
+  for (int i = 0; i < 10; i++) {
+    x += i;
+    INJECTION_LOOP_ITER("LIBMESH","SanityCheck", "inner");
+  }
+
+  INJECTION_LOOP_END("LIBMESH","SanityCheck");
+  return x;
+}
+
