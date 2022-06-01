e26c087a5f847e864437d6b13330de46ccfd4391
ben <boneill@rnet-tech.com>
2022-06-01 16:01:52 -0400

    updates

--- src/utils/vnv.C
+++ src/utils/vnv.C
@@ -17,21 +17,3 @@ INJECTION_OPTIONS(LIBMESH, libmesh_vnv_schema) {
 
 INJECTION_SUBPACKAGE(LIBMESH,PETSC)
 
-
-/** For testing purposes only -- if you are seeing this, i forgot to remove it
- *
- */
-
-int libmesh_vnv_test_function(int x) {
-   
-  INJECTION_LOOP_BEGIN("LIBMESH", VWORLD, "SanityCheck", x);
-  
-  for (int i = 0; i < 10; i++) {
-    x += i;
-    INJECTION_LOOP_ITER("LIBMESH","SanityCheck", "inner");
-  }
-
-  INJECTION_LOOP_END("LIBMESH","SanityCheck");
-  return x;
-}
-
